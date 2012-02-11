f00a09d740fb58ff0b8cf9e55981590feb4d58be
unknown <Eduardo@Porto.(none)>
2012-02-11 19:04:47 -0200

    created fault controler and domain

--- gambetta-system/grails-app/domain/gambetta/system/Fault.groovy
+++ gambetta-system/grails-app/domain/gambetta/system/Fault.groovy
@@ -1,7 +1,11 @@
 package gambetta.system
 
 class Fault {
-
-    static constraints = {
-    }
+	String name
+	Float price
+	
+	static constraints = {
+		name(maxLength:50,blank:false)
+		price(min:0.5f,max:999.99f)
+	}
 }
