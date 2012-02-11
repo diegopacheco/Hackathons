c9529b3a5c158ac113b777971f1e1cbdaee7a6e9
diego.pacheco <diego.pacheco.it@gmail.com>
2012-02-11 19:28:50 -0200

    work

--- gambetta-system/grails-app/domain/workarounder.groovy
+++ gambetta-system/grails-app/domain/workarounder.groovy
@@ -1,5 +1,16 @@
+import gambetta.system.Fault
+import java.util.List
+
 public class Workarounder {
 	String name
 	String email
 	String twitter	
+	Float  totalDebits
+	Float  currentdebits
+	List<Fault>  fault
+	
+	String toString(){
+		"${this.name},${this.email},${this.twitter}" 
+	}	
+	
 }
