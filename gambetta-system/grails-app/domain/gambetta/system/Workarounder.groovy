74aba1a4c34c69e762dee762938ebbea3cd3c0e4
diego.pacheco <diego.pacheco.it@gmail.com>
2012-02-12 15:49:47 -0200

    Refactor domain to use the same package.

---
+++ gambetta-system/grails-app/domain/gambetta/system/Workarounder.groovy
@@ -0,0 +1,19 @@
+package gambetta.system
+
+import java.util.List
+
+public class Workarounder {
+	String name
+	String email
+	String twitter	
+	Float  totalDebits
+	Float  currentdebits
+	
+	static constraints = {
+		name(blank:false)
+		email(blank:false)
+		totalDebits(min:0.1f)
+		currentdebits(min:0.1f)
+	}
+	
+}
