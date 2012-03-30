fdecf23571e8a9c0dcc28c0070f0a03ec8d570e0
Marcelo Schmidt <schmidt.marcelo@gmail.com>
2012-03-30 19:52:13 -0300

    Adding debit view

---
+++ gambetta-system/test/unit/gambetta/system/DebitControllerTests.groovy
@@ -0,0 +1,17 @@
+package gambetta.system
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(DebitController)
+class DebitControllerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
