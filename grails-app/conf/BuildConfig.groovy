6ec77b6d110badd96dcb0922822e4d2765122322
Christophe Marchal <toff63@gmail.com>
2012-05-11 19:52:44 -0300

    Added config for Heroku

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -30,20 +30,24 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
     }
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-		
-        // runtime 'mysql:mysql-connector-java:5.1.16'
-		runtime 'junit:junit:4.7'
-		runtime 'javax.activation:activation:1.1.1'
-	    runtime 'javax.mail:mail:1.4.3'
-		runtime 'org.twitter4j:twitter4j-core:2.2.5'
-    }
+		dependencies {
+			// specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+
+			// runtime 'mysql:mysql-connector-java:5.1.16'
+			runtime 'junit:junit:4.7'
+				runtime 'javax.activation:activation:1.1.1'
+				runtime 'javax.mail:mail:1.4.3'
+				runtime 'org.twitter4j:twitter4j-core:2.2.5'
+				runtime 'postgresql:postgresql:8.4-702.jdbc3'
+		}
 
     plugins {
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.7.1"
         runtime ":resources:1.1.5"
+				compile ":webxml:1.4.1"
+				compile ':heroku:1.0.1'
+				compile ':cloud-support:1.0.8'
 
         build ":tomcat:$grailsVersion"
     }
