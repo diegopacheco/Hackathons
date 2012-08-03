bc36da8d6589388e8bdeed4c84d67f318573c47d
Alexandre Poletto <alexandrepoletto@gmail.com>
2012-08-03 20:46:54 -0300

    new cowboy in flask on rails version

--- faceputti/api-python/cowboyinflask.py
+++ faceputti/api-python/cowboyinflask.py
@@ -5,20 +5,35 @@ from flask import Flask
 from pymongo import Connection
 
 app = Flask(__name__)
-
+connection = Connection()
+	
 @app.route("/api/badges/<id>")
 def retrieve_badge(id):
     return "http://localhost:80/gui/images/" + id + ".png"
 
 @app.route("/api/people/all")
 def retrieve_people_all():
-	connection = Connection()
 	db = connection['cowboy-database']
 	collection = db['cowboy-collection']
-	user = {"name": "Poletto", "date": datetime.datetime.utcnow()}
+	x = ""
+	for gambero in collection.find():
+	 x = x + json.dumps(gambero, default=json_util.default)
+	return x
+	
+	
+def load():
+	connection.drop_database("cowboy-database")
+	db = connection['cowboy-database']
+	collection = db['cowboy-collection']
+	user = {"name": "Jose Porto",
+			"rank": "24",
+			"badges": ["killer","cooker"]}
 	collection.insert(user)
-	u = collection.find_one({"name": "Poletto"})
-	return json.dumps(u, default=json_util.default)
-		
+	user2 = {"name": "Jeferson Machado(Putotti)",
+			"rank": "25",
+			"badges": ["cowboy","cooker"]}
+	collection.insert(user2)
+
 if __name__ == "__main__":
-    app.run()
+	load()
+	app.run()
