dc9caa7e96106a41873bbcd809a24de3e3e6b48a
diego.pacheco <diego.pacheco.it@gmail.com>
2012-08-03 19:49:25 -0300

    api kicking ass
    uahauhauahuahaua

--- faceputti/api-python/main_api.py
+++ faceputti/api-python/main_api.py
@@ -5,5 +5,24 @@ app = Flask(__name__)
 def retrieve_badge(id):
     return "http://localhost:80/gui/images/" + id + ".png"
 
+@app.route("/api/people/all")
+def retrieve_people_all():
+    return """
+			[
+			  {
+				"id": "1",
+				"name": "Jose Porto",
+				"rank": "24",
+				"badges": ["killer","cooker"]
+			  },
+			  {
+				"id": "2",
+				"name": "Jeferson Machado(Putotti)",
+				"rank": "25",
+				"badges": ["cooker","cowboy"]
+			  }
+			]
+			"""
+		
 if __name__ == "__main__":
     app.run()
