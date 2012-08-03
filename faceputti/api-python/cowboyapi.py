7582e1ad5d48afbf0457a456c02099db7b9ac75a
Alexandre Poletto <alexandrepoletto@gmail.com>
2012-08-03 19:53:26 -0300

    cowby apanhando

--- faceputti/api-python/cowboyapi.py
+++ faceputti/api-python/cowboyapi.py
@@ -1,13 +1,19 @@
+import datetime
 from twisted.web import server, resource
 from twisted.internet import reactor
+from pymongo import Connection
 
 class CowboyResource(resource.Resource):
-    isLeaf = True
-    numberRequests = 0
+	isLeaf = True
     
-    def render_GET(self, request):
-        request.setHeader("content-type", "text/plain")
-        return "I am cowboy bicth\n"
+	def render_GET(self, request):
+		request.setHeader("content-type", "application/json")
+		connection = Connection()
+		db = connection['cowboy-database']
+		collection = db['cowboy-collection']
+		user = {"name": "Poletto", "date": datetime.datetime.utcnow()}
+		collection.insert(user)
+		return collection.find_one({"name": "Poletto"})
 
 reactor.listenTCP(8080, server.Site(CowboyResource()))
 reactor.run()
