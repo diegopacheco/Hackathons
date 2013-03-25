3c76678e2390e7da65f0755204409a15158db36d
diego.pacheco <diego.pacheco.it@gmail.com>
2013-03-25 20:29:19 -0300

    test
    test

---
+++ warBook/app/controllers/GuerreiroController.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import models.Guerreiro
+
+object GuerreiroController extends Controller {
+  
+  def guerreiro       	=  TODO
+   
+  def newGuerreiro      =  TODO
+   
+  def deleteGuerreiro(id: Long) = TODO
+  
+}
