ff6a177b9c328cee31c83a9fbe1502e0d14cb07f
Christophe Marchal <toff63@gmail.com>
2013-03-25 22:29:40 -0300

    Added dao implementation saving into database using anom

--- warBook/app/models/Achievement.scala
+++ warBook/app/models/Achievement.scala
@@ -1,11 +1,31 @@
 package models
 
+import anorm._
+import anorm.SqlParser._
+
 case class Achievement(id: Long, label: String) 
 
 object Achievement{
- 
-  def all(): List[Achievement] = Nil
-  
-  def create(label: String) {}
-  
+	import play.api.db._
+	import play.api.Play.current
+
+	val achievement = {
+			get[Long]("id") ~ 
+			get[String]("label") map {
+			case id~label => Achievement(id, label)
+			}
+	}
+
+	def all(): List[Achievement] = DB.withConnection{ implicit c =>
+		SQL("select * from achievement").as(achievement *)
+	}
+
+	def create(label: String) {
+		DB.withTransaction { implicit c =>
+		SQL("insert into achievement (label) values ({label})").on(
+				'label -> label
+				).executeUpdate()
+		}
+	}
+
 }
